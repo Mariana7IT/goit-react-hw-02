f4c0326502d6181e2bd523ed6da90afd3ddcceea
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-07-06 12:03:38 +0300

    cng

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,68 @@
-import Profile from '../components/Profile/Profile.jsx';
-import TransactionHistory from '../components/TransactionHistory/TransactionHistory.jsx';
-import FriendList from '../components/FriendList/FriendList.jsx';
-import FriendListItem from '../components/FriendListItem/FriendListItem.jsx'
-import userData from '../userData.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
+import React, { useState } from "react";
+import Feedback from "../components/Feedback/Feedback.jsx";
+import Options from "../components/Options/Options.jsx";
+import Description from "../components/Description/Description.jsx";
+import Notification from "../components/Notification/Notification.jsx";
+import { useEffect } from "react";
 
+function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedData = JSON.parse(window.localStorage.getItem("feedback"));
+    if (savedData) {
+      return savedData;
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
 
-const App = () => {
-    return (
-        <>
-            <Profile
-                name={userData.username}
-                tag={userData.tag}
-                location={userData.location}
-                image={userData.avatar}
-                stats={userData.stats}
-            />
-            <FriendList friends={friends} />
-            <TransactionHistory transactions={transactions} />
-        </>
-    );
-};
+  useEffect(() => {
+    window.localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prev) => ({
+      ...prev,
+      [feedbackType]: prev[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
+  return (
+    <>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={totalFeedback}
+          positive={positivePercentage}
+        />
+      ) : (
+        <Notification message="No feedback yet" />
+      )}
+    </>
+  );
+}
 
 export default App;
